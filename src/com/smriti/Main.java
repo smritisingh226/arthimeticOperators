4eef9fac059f682ed2e9b8c883a2f686ccf437aa
smriti <smritisingh226@gmail.com>
2018-09-05 20:40:09 +0530

    C:\Users\smrit\OneDrive\Desktop\Workspace\arthimeticOperators\.git

---
+++ src/com/smriti/Main.java
@@ -0,0 +1,16 @@
+package com.smriti;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 100;
+        System.out.println("A in bytes: " + (byte)a);
+        int b = a *2;
+        int c = a + b;
+        int e = 15-c;
+        System.out.println("output of a is "+a);
+        System.out.println("Output of b is "+b);
+        System.out.println("Output of c is "+c);
+        System.out.println("Output of e is "+e);
+}}
+
